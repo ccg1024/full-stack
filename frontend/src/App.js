fcd28128ab2d672a347264cb294ccd53dc0ca831
ccg1024 <aimerchenyuan@163.com>
2022-09-18 11:41:10 +0800

    add 3d stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,26 +13,40 @@ import Paragraph from './libs/paragraph';
 import { ChevronRightIcon } from '@chakra-ui/icons';
 import { BioSection, BioYear } from './libs/bio';
 import Navbar from './components/Navbar';
+import { AnimatePresence, motion } from 'framer-motion'
+import VoxelDog from './components/voxel-dog';
 
 
 const App = () => {
   return (
     <>
-
       <Navbar path="/" />
+      <VoxelDog />
       <Container>
-        <Box borderRadius='lg' bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} mb={6} p={3} align='center'>
-          This is a full stack project, using React and Spring boot.
-        </Box>
+        <AnimatePresence
+        exitBeforeEnter
+        initial={true}
+        >
+          <motion.div
+          initial={{ y: 20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -20, opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          >
+            <Box borderRadius='lg' bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} mb={6} p={3} align='center'>
+              This is a full stack project, using React and Spring boot.
+            </Box>
 
-        <Box display={{ md: 'flex' }}>
-          <Box flexGrow={1} >
-            <Heading>
-              William Home
-            </Heading>
-            <p>a small descript information.</p>
-          </Box>
-        </Box>
+            <Box display={{ md: 'flex' }}>
+              <Box flexGrow={1} >
+                <Heading>
+                  William Home
+                </Heading>
+                <p>a small descript information.</p>
+              </Box>
+            </Box>
+          </motion.div>
+        </AnimatePresence>
         
         <Section delay={0.1}>
           <Heading as="h3" variant="section-title">
