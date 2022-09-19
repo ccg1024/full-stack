6e066f0c8f1b377104adc7bb2d38dc7b113807e2
ccg1024 <aimerchenyuan@163.com>
2022-09-20 00:18:58 +0800

    change web title and add markdown page

---
+++ frontend/src/components/grid-item.jsx
@@ -0,0 +1,41 @@
+import { Box, Text, LinkBox, LinkOverlay, Image } from '@chakra-ui/react'
+
+
+export const GridItem = ({ children, href, title, thumbnail }) => (
+  <Box w="100%" align="center">
+    <LinkBox cursor="pointer">
+      <Image
+        src={thumbnail}
+        alt={title}
+        placeholder='blur'
+        loading='lazy'
+        style={{ borderRadius: '12px' }}
+        />
+      <LinkOverlay href={href} target="_blank">
+        <Text mt={2}>{title}</Text>
+      </LinkOverlay>
+      <Text fontSize={14}>{children}</Text>
+    </LinkBox>
+  </Box>
+)
+// target="_blank" open link in new window
+
+export const MarkDownGridItem = ({ children, id, title, thumbnail }) => (
+  <Box w="100%" align="center">
+    <LinkBox cursor="pointer">
+      <Image
+        src={thumbnail}
+        alt={title}
+        placeholder='blur'
+        style={{ borderRadius: '12px' }}
+        />
+      <LinkOverlay href={`/markdown/${id}`}>
+        <Text mt={2} fontSize={20}>
+          {title}
+        </Text>
+      </LinkOverlay>
+      <Text fontSize={14}>{children}</Text>
+    </LinkBox>
+  </Box>
+)
+
--- frontend/src/markdown.js
+++ frontend/src/markdown.js
@@ -1,12 +1,59 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import VoxelDog from "./components/voxel-dog";
+import {
+  Container,
+  Heading,
+  SimpleGrid
+} from '@chakra-ui/react'
+import Section from "./libs/sections";
+import { MarkDownGridItem } from "./components/grid-item";
+import { AnimatePresence, motion } from "framer-motion"
 
 
 const Markdown = () => {
   return (
     <>
       <Navbar path='/markdown'/>
-      <p>This is markdown page</p>
+      <VoxelDog />
+      <Container>
+        <AnimatePresence
+          exitBeforeEnter
+          initial={true}
+        >
+          <motion.div
+            initial={{ y: 20, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: -20, opacity: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Heading as="h3" mb={4}>
+              Markdown
+            </Heading>
+
+            <SimpleGrid columns={[1, 1, 2]} gap={6}>
+              <Section>
+                <MarkDownGridItem
+                  id='markdown_test'
+                  title='markdown_test'
+                  thumbnail='/images/markdown/markdown.png'>
+                  this is a describtion for card
+                </MarkDownGridItem>
+              </Section>
+
+              <Section>
+                <MarkDownGridItem
+                  id='markdown'
+                  title='markdown'
+                  thumbnail='/images/markdown/markdown.png'>
+                  this is a describtion for card
+                </MarkDownGridItem>
+
+              </Section>
+            </SimpleGrid>
+          </motion.div>
+        </AnimatePresence>
+      </Container>
     </>
   )
 }
