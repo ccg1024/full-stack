c747428dd806b5be81a6748aed72b4c02242b107
ccg <aimerchenyuan@163.com>
2022-10-20 15:02:28 +0800

    add link, img, quote and a bit of code area

--- frontend/src/components/show-markdow.jsx
+++ frontend/src/components/show-markdow.jsx
@@ -4,15 +4,47 @@ import {
   Heading,
   Box,
   Text,
-  useColorModeValue
+  useColorModeValue,
+  Link,
+  Image
 } from '@chakra-ui/react'
 import MarkdownContentService from '../services/MarkdownContentService'
+import Editor from 'react-simple-code-editor'
+import { highlight, languages } from 'prismjs/components/prism-core'
+import 'prismjs/components/prism-clike'
+import 'prismjs/components/prism-javascript'
+import 'prismjs/themes/prism.css' //Example style, you can use another
 
 const MarkdownBanner = () => {
   return (
     <Box borderRadius='lg' bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} mb={6} p={3} align='center'>
       <Heading as="h5" fontSize={20}>Show markdown container</Heading>
     </Box>
+  )
+}
+
+const QuoteContent = ({props, children}) => {
+  const key = props
+  return (
+    <Box borderRadius='sm' bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} key={key} mt={2} mb={2} pt={2} pb={2}>
+      <Text key={key}>{children}</Text>
+    </Box>
+  )
+}
+
+const MyCodeArea = () => {
+  const [code, setCode] = React.useState(`function add(a, b) {\n  return a + b;\n}`)
+  return (
+    <Editor
+      value={code}
+      onValueChange={code => setCode(code)}
+      highlight={code => highlight(code, languages.js)}
+      padding={10}
+      style={{
+        fontFamily: '"Fira code", "Fira Mono", monospace',
+        fontSize: 12,
+      }}
+      />
   )
 }
 
@@ -50,7 +82,16 @@ class ShowMarkdown extends Component {
                 return <Heading as='h5' key={index} fontSize={20}>{ item.content }</Heading>
               } else if (item.tag === 'h6') {
                 return <Heading as='h6' key={index} fontSize={10}>{ item.content }</Heading>
+              } else if (item.tag === 'link') {
+                return <Link href={item.href} key={index}>{ item.content }</Link>
+              } else if (item.tag === 'img') {
+                return <Image src={item.src} alt={item.alt} key={index}/>
+              } else if (item.tag === 'quote') {
+                return <QuoteContent key={index}>{ item.content }</QuoteContent>
+              } else if (item.tag === 'code-area') {
+                return <MyCodeArea/>
               }
+              return <Text key={index}> The tag { item.tag } is not implement.</Text>
             })
           }
           </Box>
