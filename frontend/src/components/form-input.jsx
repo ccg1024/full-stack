8b51d0f4272d89bb9c52ff9645234f12067bfd18
ccg <aimerchenyuan@163.com>
2023-04-01 11:34:30 +0800

    extract common component from login.jsx

--- frontend/src/components/form-input.jsx
+++ frontend/src/components/form-input.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
-import { Input, Button, useColorModeValue, Box, Text } from '@chakra-ui/react'
+import {
+  Input,
+  Button,
+  useColorModeValue,
+  Box,
+  Text,
+  Container,
+  Flex,
+  Divider
+} from '@chakra-ui/react'
+import { CloseIcon } from '@chakra-ui/icons'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export const FormInput = ({ name, type, placeholder, onChange }) => {
   return (
@@ -41,5 +52,67 @@ export const FormErrorMessage = ({ children }) => {
         {children}
       </Text>
     </Box>
+  )
+}
+
+export const FormContainer = ({
+  keyId,
+  isVisible,
+  setIsVisible,
+  colors,
+  modalHead,
+  children
+}) => {
+  const closeModal = () => setIsVisible(false)
+  return (
+    <AnimatePresence>
+      {isVisible && (
+        <motion.div
+          key={keyId}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Container
+            maxW="container.md"
+            backgroundColor={colors.containerBackground}
+            position="absolute"
+            left="50%"
+            top="100px"
+            transform="translate(-50%, 0%)"
+            borderRadius="md"
+            boxShadow={colors.containerBoxShadow}
+            color={colors.containerColor}
+            zIndex={10}
+            transition="display 0.5s"
+          >
+            <Box padding={{ base: 2, md: 6 }} paddingX={{ md: 10 }}>
+              <Flex justifyContent="right">
+                <Box>
+                  <CloseIcon
+                    _hover={{
+                      cursor: 'pointer',
+                      transform: 'scale(1.5)',
+                      transition: 'transform 0.5s'
+                    }}
+                    transition="transform 0.5s"
+                    onClick={closeModal}
+                  />
+                </Box>
+              </Flex>
+              <Box textAlign={'center'}>
+                <Text fontWeight="bold" fontSize="1.2rem" my={4}>
+                  {modalHead}
+                </Text>
+              </Box>
+
+              <Divider mb={4} borderColor={colors.dividerBorderColor} />
+              {children}
+            </Box>
+          </Container>
+        </motion.div>
+      )}
+    </AnimatePresence>
   )
 }
--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,28 +1,29 @@
 import _ from 'lodash'
 import axios from 'axios'
 import PubSub from 'pubsub-js'
-import { CloseIcon } from '@chakra-ui/icons'
 import React, { useState, useEffect } from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
 import {
   Box,
-  Container,
   FormControl,
   FormLabel,
   Text,
-  Divider,
   Flex,
   Link,
   useColorModeValue
 } from '@chakra-ui/react'
 
 import { pubsubPipe } from '../config/pubsub'
-import { FormInput, FormButton, FormErrorMessage } from './form-input'
+import {
+  FormInput,
+  FormButton,
+  FormErrorMessage,
+  FormContainer
+} from './form-input'
 
 const Login = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [isVisible, setIsVisible] = useState(true)
+  const [isVisible, setIsVisible] = useState(false)
   const [userCharErr, setUserCharErr] = useState('')
   const [pswdCharErr, setPswdCharErr] = useState('')
   const [loginMessage, setLoginMessage] = useState('')
@@ -36,7 +37,6 @@ const Login = () => {
 
   const submitData = e => {
     e.preventDefault()
-    e.target.disabled = true
     console.log(username)
     console.log(password)
 
@@ -47,13 +47,16 @@ const Login = () => {
     // simple validation of input legitimacy
     if (!/^[a-zA-z0-9_]+$/.test(username)) {
       setUserCharErr('username is empty or include invalide char')
+      return
     }
 
     if (!/^[a-zA-z0-9_]+$/.test(password)) {
       setPswdCharErr('password is empty or include invalide char')
+      return
     }
 
-    // using axios
+    // using axios and diable button
+    e.target.disabled = true
     axios
       .post('http://127.0.0.1:5000/api/login', { username, password })
       .then(res => {
@@ -75,10 +78,6 @@ const Login = () => {
       })
   }
 
-  const closeLogin = () => {
-    setIsVisible(false)
-  }
-
   useEffect(() => {
     const token = PubSub.subscribe(pubsubPipe.loginRegiste, (_msg, data) => {
       if (data === 'login') {
@@ -92,6 +91,10 @@ const Login = () => {
     }
   }, [])
 
+  const clickRegiste = () => {
+    PubSub.publish(pubsubPipe.loginRegiste, 'registe')
+  }
+
   const colors = {
     containerBackground: useColorModeValue('#202023', 'green.200'),
     containerBoxShadow: useColorModeValue('dark-lg', 'none'),
@@ -102,102 +105,61 @@ const Login = () => {
 
   return (
     <>
-      <AnimatePresence model="wait" initial={true}>
-        {isVisible && (
-          <motion.div
-            key="login"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.5 }}
-          >
-            <Container
-              maxW="container.md"
-              backgroundColor={colors.containerBackground}
-              position="absolute"
-              left="50%"
-              top="100px"
-              transform="translate(-50%, 0%)"
-              borderRadius="md"
-              boxShadow={colors.containerBoxShadow}
-              color={colors.containerColor}
-              zIndex={10}
-              transition="display 0.5s"
-            >
-              <Box padding={{ base: 2, md: 6 }} paddingX={{ md: 10 }}>
-                <Flex justifyContent="right">
-                  <Box>
-                    <CloseIcon
-                      _hover={{
-                        cursor: 'pointer',
-                        transform: 'scale(1.5)',
-                        transition: 'transform 0.5s'
-                      }}
-                      transition="transform 0.5s"
-                      onClick={closeLogin}
-                    />
-                  </Box>
-                </Flex>
-                <Box textAlign={'center'}>
-                  <Text fontWeight="bold" fontSize="1.2rem" my={4}>
-                    Login
-                  </Text>
-                </Box>
+      <FormContainer
+        keyId="login"
+        isVisible={isVisible}
+        setIsVisible={setIsVisible}
+        colors={colors}
+        modalHead="Login"
+      >
+        <form>
+          <FormControl>
+            <FormLabel fontWeight="bold">User Name</FormLabel>
+            <FormInput
+              type="text"
+              name="username"
+              placeholder="username"
+              onChange={_.debounce(usernameChange, 16)}
+            />
+            {userCharErr === '' ? (
+              ''
+            ) : (
+              <FormErrorMessage>{userCharErr}</FormErrorMessage>
+            )}
+            <FormLabel fontWeight="bold">Passworld</FormLabel>
+            <FormInput
+              type="password"
+              name="password"
+              placeholder="password"
+              onChange={_.debounce(passwordChange, 16)}
+            />
+            {pswdCharErr === '' ? (
+              ''
+            ) : (
+              <FormErrorMessage>{pswdCharErr}</FormErrorMessage>
+            )}
+          </FormControl>
 
-                <Divider mb={4} borderColor={colors.dividerBorderColor} />
+          {loginMessage ? (
+            <FormErrorMessage>{loginMessage}</FormErrorMessage>
+          ) : (
+            ''
+          )}
 
-                <form>
-                  <FormControl>
-                    <FormLabel fontWeight="bold">User Name</FormLabel>
-                    <FormInput
-                      type="text"
-                      name="username"
-                      placeholder="username"
-                      onChange={_.debounce(usernameChange, 16)}
-                    />
-                    {userCharErr === '' ? (
-                      ''
-                    ) : (
-                      <FormErrorMessage>{userCharErr}</FormErrorMessage>
-                    )}
-                    <FormLabel fontWeight="bold">Passworld</FormLabel>
-                    <FormInput
-                      type="password"
-                      name="password"
-                      placeholder="password"
-                      onChange={_.debounce(passwordChange, 16)}
-                    />
-                    {pswdCharErr === '' ? (
-                      ''
-                    ) : (
-                      <FormErrorMessage>{pswdCharErr}</FormErrorMessage>
-                    )}
-                  </FormControl>
+          <FormButton onClick={submitData}>Login</FormButton>
+        </form>
 
-                  {loginMessage ? (
-                    <FormErrorMessage>{loginMessage}</FormErrorMessage>
-                  ) : (
-                    ''
-                  )}
-
-                  <FormButton onClick={submitData}>Login</FormButton>
-                </form>
-
-                <Flex mt={4} mb={4} justifyContent="space-between">
-                  <Box>
-                    <Text>forgot password?</Text>
-                  </Box>
-                  <Box>
-                    <Link href="#" color={colors.linkColor}>
-                      registe
-                    </Link>
-                  </Box>
-                </Flex>
-              </Box>
-            </Container>
-          </motion.div>
-        )}
-      </AnimatePresence>
+        <Flex mt={4} mb={4} justifyContent="space-between">
+          <Box>
+            <Text>forgot password?</Text>
+          </Box>
+          <Box>
+            <Link href="#" color={colors.linkColor} onClick={clickRegiste}>
+              registe
+            </Link>
+          </Box>
+        </Flex>
+      </FormContainer>
     </>
   )
 }
